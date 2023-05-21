42ce129b2962d3d7965b6f9ca78116f75007b80e
Gabriel Pimentel Dolzan <gabrieldolzan@icloud.com>
2023-05-20 21:09:53 -0300

    Initial commit

---
+++ dependencies/install.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get upgrade
+sudo apt install gcc
+sudo apt install git
+sudo apt install make
+sudo apt install net-tools
---
+++ source/main.c
@@ -0,0 +1,7 @@
+#include "ConexaoRawSocket.h"
+
+int main(int argc, char const *argv[])
+{
+    ConexaoRawSocket("eno1");
+    return 0;
+}
